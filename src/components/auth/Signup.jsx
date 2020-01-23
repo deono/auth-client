409e7a74d87c1f3530990a0273b9210cb50e8668
Deon Odendaal <deon.odendaal@protonmail.com>
2020-01-23 03:56:31 +0000

    signup and signin componen scaffold

---
+++ src/components/auth/Signup.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+
+export class Signup extends Component {
+  state = {
+    email: "",
+    password: ""
+  };
+
+  handleInputChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    // dispatch action here
+  };
+  render() {
+    return (
+      <div className="container">
+        <div className="row justify-content-center align-items-center">
+          <div className="card col-10 col-sm-8 col-md-6 col-lg-4">
+            <div className="card-body">
+              <h3 className="card-title text-center">Sign Up</h3>
+              {/* Login form */}
+              <form onSubmit={this.handleSubmit}>
+                <div className="form-group">
+                  <label htmlFor="email">Email address</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="email"
+                    name="email"
+                    value={this.state.value}
+                    onChange={this.handleInputChange}
+                    aria-describedby="emailHelp"
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="password">Password</label>
+                  <input
+                    type="password"
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.handleInputChange}
+                    className="form-control"
+                    id="password"
+                  />
+                </div>
+                <div className="d-flex justify-content-end">
+                  <button class="btn btn-primary" type="submit">
+                    Submit
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Signup;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Route } from "react-router-dom";
 import App from "./components/App";
 import Welcome from "./components/Welcome";
+import Signup from "./components/auth/Signup";
 
 ReactDOM.render(
   <BrowserRouter>
     <App>
       <Route exact path="/" component={Welcome} />
+      <Route path="/signup" component={Signup} />
     </App>
   </BrowserRouter>,
   document.getElementById("root")
