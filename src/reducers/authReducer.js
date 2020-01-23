293e3d5fc37ef25502bcb53024447a40ff8e0e07
Deon Odendaal <deon.odendaal@protonmail.com>
2020-01-23 04:09:06 +0000

    redux setup

---
+++ src/reducers/authReducer.js
@@ -0,0 +1,15 @@
+const INITIAL_STATE = {
+  authenticated: "",
+  errorMessage: ""
+};
+
+export default function(state = INITIAL_STATE, action) {
+  const { type, payload } = action;
+  switch (type) {
+    case value:
+      break;
+
+    default:
+      break;
+  }
+}
